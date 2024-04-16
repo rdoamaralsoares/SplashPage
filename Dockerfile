a28bbd1053e052ac91d43db4bcffbe78287f80de
Ricardo Soares <soares.ricardo@gmail.com>
2024-04-16 17:19:08 -0300

    Version_1
    Pagina sem DB

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Use a imagem base do Python
+FROM python:3.9
+
+# Defina o diretório de trabalho dentro do contêiner
+WORKDIR /app
+
+# Copie o arquivo requirements.txt e instale as dependências
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copie todos os arquivos do diretório atual para o diretório de trabalho no contêiner
+COPY . .
+
+# Exponha a porta em que o Flask está sendo executado
+EXPOSE 5000
+
+# Comando para iniciar o aplicativo Flask quando o contêiner for iniciado
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Dados de usuários (geralmente isso seria armazenado em um banco de dados)
+usuarios = {'usuario1': 'senha1', 'usuario2': 'senha2'}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    usuario = request.form['usuario']
+    senha = request.form['senha']
+    if usuario in usuarios and usuarios[usuario] == senha:
+        return f'Bem-vindo, {usuario}!'
+    else:
+        return 'Credenciais inválidas. Por favor, tente novamente.'
+
+@app.route('/cadastro', methods=['POST'])
+def cadastro():
+    novo_usuario = request.form['novousuario']
+    nova_senha = request.form['novasenha']
+    if novo_usuario not in usuarios:
+        usuarios[novo_usuario] = nova_senha
+        return f'Usuário {novo_usuario} cadastrado com sucesso!'
+    else:
+        return 'Usuário já existe. Por favor, escolha outro nome de usuário.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
